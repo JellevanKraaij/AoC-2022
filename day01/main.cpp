c4175de9f5cbcd5ab67c4541e604f08d3ed47b79
Jelle van Kraaij <jellevankraaij@gmail.com>
2022-12-01 21:16:49 +0100

    add day01

---
+++ day01/main.cpp
@@ -0,0 +1,42 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		cout << "Usage: " << argv[0] << " inputfile" << endl;
+		return (EXIT_FAILURE);
+	}
+
+	ifstream inputFile(argv[1]);
+
+	vector<int> totals;
+	int currentTotal;
+
+	string line;
+	while (getline(inputFile, line))
+	{
+		int current = atoi(line.c_str());
+		if (current == 0)
+		{
+			totals.push_back(currentTotal);
+			currentTotal = 0;
+			continue;
+		}
+		currentTotal += current;
+	}
+	sort(totals.begin(), totals.end(), greater<int>());
+	int total = 0;
+	for (int i = 0; i < min(totals.size(), (size_t)3); i++)
+	{
+		total += totals[i];
+		cout << i + 1 << ": " << totals[i] << endl;
+	}
+	cout << "total: " << total << endl;
+}
