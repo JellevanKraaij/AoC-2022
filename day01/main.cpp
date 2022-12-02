ca197ad78e361280229b85ea86cc8c140919490e
Jelle van Kraaij <jellevankraaij@gmail.com>
2022-12-02 19:18:36 +0100

    style day01

--- day01/main.cpp
+++ day01/main.cpp
@@ -1,42 +1,37 @@
+#include <algorithm>
+#include <cstdlib>
 #include <fstream>
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <cstdlib>
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-	if (argc != 2)
-	{
-		cout << "Usage: " << argv[0] << " inputfile" << endl;
-		return (EXIT_FAILURE);
-	}
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        cout << "Usage: " << argv[0] << " inputfile" << endl;
+        return (EXIT_FAILURE);
+    }
 
-	ifstream inputFile(argv[1]);
+    ifstream inputFile(argv[1]);
 
-	vector<int> totals;
-	int currentTotal;
+    vector<int> totals;
+    int currentTotal;
 
-	string line;
-	while (getline(inputFile, line))
-	{
-		int current = atoi(line.c_str());
-		if (current == 0)
-		{
-			totals.push_back(currentTotal);
-			currentTotal = 0;
-			continue;
-		}
-		currentTotal += current;
-	}
-	sort(totals.begin(), totals.end(), greater<int>());
-	int total = 0;
-	for (int i = 0; i < min(totals.size(), (size_t)3); i++)
-	{
-		total += totals[i];
-		cout << i + 1 << ": " << totals[i] << endl;
-	}
-	cout << "total: " << total << endl;
+    string line;
+    while (getline(inputFile, line)) {
+        int current = atoi(line.c_str());
+        if (current == 0) {
+            totals.push_back(currentTotal);
+            currentTotal = 0;
+            continue;
+        }
+        currentTotal += current;
+    }
+    sort(totals.begin(), totals.end(), greater<int>());
+    int total = 0;
+    for (int i = 0; i < min(totals.size(), (size_t)3); i++) {
+        total += totals[i];
+        cout << i + 1 << ": " << totals[i] << endl;
+    }
+    cout << "total: " << total << endl;
 }
