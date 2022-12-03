3fad6cf349d828205767f19c91758328102722da
Jelle van Kraaij <jellevankraaij@gmail.com>
2022-12-03 23:58:24 +0100

    finish day03

---
+++ day03/main-0.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int calculatePrioValue(const string &input) {
+    int result = 0;
+
+    for (const char &c : input) {
+        if (islower(c))
+            result += c - 'a' + 1;
+        else if (isupper(c))
+            result += c - 'A' + 27;
+    }
+    return (result);
+}
+
+string findDoubleItems(const string &backpack1, const string &backpack2) {
+    string doubleItems;
+    string alreadyChecked;
+    for (const char &c : backpack1) {
+        if (alreadyChecked.find(c) == string::npos && backpack2.find(c) != string::npos) {
+            alreadyChecked += c;
+            doubleItems += c;
+        }
+    }
+    return (doubleItems);
+}
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " inputfile" << endl;
+        return (EXIT_FAILURE);
+    }
+
+    ifstream inputFile(argv[1]);
+    if (!inputFile) {
+        cerr << "Unable to open file " << argv[1] << endl;
+        return (EXIT_FAILURE);
+    }
+    string line;
+    int totalPrio;
+    while (getline(inputFile, line)) {
+        if ((line.length() % 2) != 0 || line.empty() || none_of(line.begin(), line.end(), ::isalpha)) {
+            cerr << "backpack format error" << endl;
+            return (EXIT_FAILURE);
+        }
+        const string backpack1 = line.substr(0, line.length() / 2);
+        const string backpack2 = line.substr(line.length() / 2);
+
+        const string doubleItems = findDoubleItems(backpack1, backpack2);
+        totalPrio += calculatePrioValue(doubleItems);
+        cout << backpack1 << " : " << backpack2 << " wrong items = " << doubleItems << endl;
+    }
+    cout << endl;
+    cout << "totalScore = " << totalPrio << endl;
+}
---
+++ day03/main-1.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <fstream>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int calculatePrioValue(string input) {
+    int result = 0;
+
+    for (const char &c : input) {
+        if (islower(c))
+            result += c - 'a' + 1;
+        else if (isupper(c))
+            result += c - 'A' + 27;
+    }
+    return (result);
+}
+
+string findSharedItems(const string input[3]) {
+    string sharedItems;
+    string alreadyChecked;
+
+    for (const char &c : input[0]) {
+        if (alreadyChecked.find(c) == string::npos && input[1].find(c) != string::npos && input[2].find(c) != string::npos) {
+            alreadyChecked += c;
+            sharedItems += c;
+        }
+    }
+    return (sharedItems);
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " inputfile" << endl;
+        return (EXIT_FAILURE);
+    }
+
+    ifstream inputFile(argv[1]);
+    if (!inputFile) {
+        cerr << "Unable to open file " << argv[1] << endl;
+        return (EXIT_FAILURE);
+    }
+    int i = 0;
+    string lines[3];
+    int totalPrio;
+    while (getline(inputFile, lines[i])) {
+        if ((lines[i].length() % 2) != 0 || lines[i].empty() || none_of(lines[i].begin(), lines[i].end(), ::isalpha)) {
+            cerr << "backpack format error" << endl;
+            return (EXIT_FAILURE);
+        }
+        if (++i < 3)
+            continue;
+        i = 0;
+        const string sharedItems = findSharedItems(lines);
+        totalPrio += calculatePrioValue(sharedItems);
+        cout << lines[0] << " : " << lines[1] << " : " << lines[2] << " shared items = " << sharedItems << endl;
+    }
+    cout << endl;
+    cout << "totalScore = " << totalPrio << endl;
+}
